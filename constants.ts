c8cbae5a6a8e127f40f6ff7dc331c7052dc6ee56
KABILOVM <106799657+KABILOVM@users.noreply.github.com>
2026-01-31 19:31:52 +0500

    12

---
+++ constants.ts
@@ -0,0 +1,27 @@
+
+export const BRAND_NAMES = [
+  'Belinda',
+  'Beauty',
+  'Style',
+  'Elegance',
+  'Premium'
+];
+
+export const COLORS = {
+  base: '#7ea4ff', 
+  bg: '#8493ff',
+  text: '#ffffff',
+  blockPrimary: '#2563eb', // Blue 600
+  blockSecondary: '#1d4ed8' // Blue 700
+};
+
+export const MAX_TRIALS = 6;
+
+export const GAME_CONFIG = {
+  BOX_HEIGHT: 1,
+  INITIAL_SIZE: 3,
+  MIN_SIZE: 0.3,
+  BASE_SPEED_INCREMENT: 0.0025,
+  MAX_SPEED: 0.075,
+  BASE_SPEED: 0.035,
+};
